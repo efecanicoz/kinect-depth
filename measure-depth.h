9e0a9f30869fb97aecf3d712113da3d5a8154573
Efe Can <efecanicoz@gmail.com>
2016-05-17 11:08:05 +0300

    depth-array file added for just getting an array of depth values and nothing more

---
+++ measure-depth.h
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "libfreenect.h"
+#include <time.h>
+
+#include <pthread.h>
+
+#if defined(__APPLE__)
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <math.h>
+
